ecc15ed64b1c1cd90b2ec6e98db8ec6ac0bd9276
Saloni <salonigupta12405@gmail.com>
2023-12-12 00:37:33 -0500

    Adding files

--- reviews/models.py
+++ reviews/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Review(models.Model):
+    name_text = models.CharField(max_length=100, blank=True)
+    email_text = models.CharField(max_length=100, blank=True)
+    phone_text = models.CharField(max_length=100, blank=True)
     review_text = models.CharField(max_length=100, blank=True)
--- reviews/views.py
+++ reviews/views.py
@@ -8,14 +8,30 @@ class AppDevClubReviewsView(APIView):
     def get(self, request):
         reviews = []
         for review in Review.objects.filter():
-            reviews.append(review.review_text)
+            reviews.append(review.name_text),
+            reviews.append(review.email_text),
+            reviews.append(review.phone_text),
+            reviews.append(review.review_text),
+            
+        reviews = list(filter(None, reviews))
+        reviews = list(set(reviews))
         return Response({'reviews': reviews})
 
 class CreateAppDevClubReview(APIView):
     def post(self, request):
+        name = request.data['name']
+        email = request.data['email']
+        phone = request.data['phone']
         review = request.data['review']
-        if review != '':
+        if name != '':
+            new_database_entry = Review(name_text=name)
+        if email != '':
+            new_database_entry = Review(email_text=email)
+        if phone != '':
+            new_database_entry = Review(phone_text=phone)
+        if name != '':
             new_database_entry = Review(review_text=review)
+
             new_database_entry.save()
             return Response({'message': 'success'})
         else:
